ab7617976b8f863d0b1f1e7f5992776d20dfd155
Eric Kurtz <erickurtz@live.com>
2017-11-25 19:00:18 -0500

    small changes

---
+++ src/Job.java
@@ -0,0 +1,21 @@
+
+public class Job {
+
+	int priority; 
+	int arrivTime; 
+	int memReq; 
+	int dev; 
+	int runTime; 
+	
+	public Job(int p, int a, int m, int d, int r) {
+		
+		this.priority = p; 
+		this.arrivTime = a;
+		this.memReq = m;
+		this.dev = d;
+		this. runTime = r; 
+	}
+	
+	
+	
+} 
